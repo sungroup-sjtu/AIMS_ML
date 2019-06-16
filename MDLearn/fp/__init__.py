31851c41751483a0a83aa8508c7290c90e2452e0
Zheng Gong <unimisia@gmail.com>
2019-06-16 21:42:22 +0800

    Remove MDLearn dir

--- MDLearn/fp/__init__.py
+++
@@ -1,9 +0,0 @@
-from .fp import Fingerprint
-from .wyz import WyzIndexer
-from .simple import SimpleIndexer
-from .rdk import ECFP4Indexer, MorganCountIndexer, Morgan1CountIndexer, PredefinedMorganCountIndexer, PredefinedMorgan1CountIndexer
-
-encoders_dict = dict([(e.name, e) for e in (WyzIndexer, SimpleIndexer,
-                                            ECFP4Indexer, MorganCountIndexer, Morgan1CountIndexer,
-                                            PredefinedMorganCountIndexer, PredefinedMorgan1CountIndexer
-                                            )])
