c249ed3a7f9e30eaf63dad9f6e4309e9f6f073a0
Zheng Gong <unimisia@gmail.com>
2019-07-11 19:08:18 +0800

    Sort ECFP fingerprint

--- mdlearn/fp/__init__.py
+++ mdlearn/fp/__init__.py
@@ -1,9 +1,9 @@
 from .fp import Fingerprint
 from .wyz import WyzIndexer
-from .simple import SimpleIndexer
+from .simple import SimpleIndexer, ExtraIndexer
 from .rdk import ECFP4Indexer, MorganCountIndexer, Morgan1CountIndexer, PredefinedMorganCountIndexer, PredefinedMorgan1CountIndexer
 
-encoders_dict = dict([(e.name, e) for e in (WyzIndexer, SimpleIndexer,
+encoders_dict = dict([(e.name, e) for e in (WyzIndexer, SimpleIndexer, ExtraIndexer,
                                             ECFP4Indexer, MorganCountIndexer, Morgan1CountIndexer,
                                             PredefinedMorganCountIndexer, PredefinedMorgan1CountIndexer
                                             )])
