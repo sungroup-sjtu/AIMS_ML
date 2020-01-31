7403173d5114d0e1652c1db4c2ee1b6d6ab40f55
XiangyanSJTU <hnxxxy123@sjtu.edu.cn>
2020-01-31 18:06:10 -0500

    bugfix

--- mdlearn/fp/__init__.py
+++ mdlearn/fp/__init__.py
@@ -4,7 +4,7 @@ from .simple import SimpleIndexer, ExtraIndexer
 from .rdk import *
 
 encoders_dict = dict([(e.name, e) for e in (WyzIndexer, SimpleIndexer, ExtraIndexer,
-                                            ECFP4Indexer, MorganCountIndexer, Morgan1CountIndexer,
+                                            ECFPIndexer, MorganCountIndexer, Morgan1CountIndexer,
                                             PredefinedMorganCountIndexer, PredefinedMorgan1CountIndexer,
                                             TopologicalCountIndexer, PredefinedTopologicalCountIndexer
                                             )])
